8b97dc9c9bc4bd25f58e3137542f5a7e12472f15
mohamadatef <m.atef.radwan@gmail.com>
2018-01-08 21:26:02 +0300

    adding forms with tempaltes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-#app here is foldder name not module name 
+#app here is directory name name not module name 
+app.config['SECRET_KEY']= "OIunsb7??=-0982+++s"
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,16 @@
 #ffrom app floder import app application __init__
 from flask import render_template
 from app import app
+from app.form import LoginForm
 @app.route("/")
 @app.route("/index")
 def index():
     data  = {'username' : "Mohamad Radwan"}
     return render_template('index.html', user  = data ,title = "Home Page")
-
-
+@app.route("/user")
+def user():
+    return render_template("user.html", title = "User page Title", user = "Radwan")
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", form = form)
