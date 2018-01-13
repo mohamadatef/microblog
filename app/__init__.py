7fd93f47b8fd9e5ded994c2eac386a749688c750
Mohamad Radwan <mohamad@localhost.localdomain>
2018-01-13 08:44:55 +0300

    Linux Last mods

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 #app here is directory name name not module name 
 app.config['SECRET_KEY']= "OIunsb7??=-0982+++s"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-#ffrom app floder import app application __init__
-from flask import render_template
+#from app floder import app application __init__
+from flask import render_template, flash , redirect, url_for
+
 from app import app
 from app.form import LoginForm
 @app.route("/")
@@ -10,7 +11,16 @@ def index():
 @app.route("/user")
 def user():
     return render_template("user.html", title = "User page Title", user = "Radwan")
-@app.route("/login")
+@app.route("/login", methods = ["POST", "GET"])
+
+#----------------------------------#
+#Start Login Route Here
 def login():
     form = LoginForm()
+    
+    if form.validate_on_submit():
+        flash("Login requestd")
+    
+        return redirect(url_for("index"))
+
     return render_template("login.html", form = form)
